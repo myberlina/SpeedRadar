7675807a3015b8462f47f074f0bba205d1cc2ca2
David Deaves <HSV_Timing@dd.id.au>
2025-11-22 21:42:27 +1100

    Change the do_shutdown.php to force to home
    To prevent an accidental reload after power off causing
    another poweroff schedule the page to load home 7sec after
    a successful power off.  Or 20sec after a failed attempt.

--- html/do_shutdown.php
+++ html/do_shutdown.php
@@ -4,16 +4,23 @@
 <html>
  <head>
   <title>Shutdown</title>
+  <script type="text/javascript">
+    function return_home() { document.location="/"; };
+    setTimeout(return_home, 20000);
+  </script>
  </head>
  <body>
   <h2>System Shutting Down</h2>
   <?php
-    if (!(false === exec("sudo /usr/sbin/poweroff", $results, $rc)) && ($rc == 0)) {
+    if (!(false === exec("sudo /usr/sbin/poweroff 2>&1", $results, $rc)) && ($rc == 0)) {
         echo "Shutdown initiated";
+        echo "<script type=\"text/javascript\"> setTimeout(return_home, 7000);</script>";
     } 
     else {
-        echo "Shutdown command failed";
-        foreach($results as $num => $line) echo "$line<br>";
+        echo "Shutdown command failed -- $rc<br>";
+        foreach($results as $num => $line) echo "<br>$line";
+	echo "<p><br></p>";
+	echo "<p><a href=\"/\">Home</a></p>";
     } 
   ?>
  </body>
